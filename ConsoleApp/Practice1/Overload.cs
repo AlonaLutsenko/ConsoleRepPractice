b867d02a5f98f90dbc73b5e8fc030fe4bd3927e1
AlonaLutsenko <alonalutsenkobb@gmail.com>
2021-07-31 18:55:02 +0300

    first

---
+++ ConsoleApp/Practice1/Overload.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Practice1
+{
+    public class Overload
+    {
+        private string name = "Akhil";
+
+        public void Display()
+        {
+            Display2(ref name, ref name);
+            System.Console.WriteLine(name);
+        }
+
+        private void Display2(ref string x, ref string y)
+        {
+            System.Console.WriteLine(name);
+            x = "Akhil 1";
+            System.Console.WriteLine(name);
+            y = "Akhil 2";
+            System.Console.WriteLine(name);
+            name = "Akhil 3";
+        }
+    }
+}
---
+++ ConsoleApp/Practice1/Program.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleApp.Practice1;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Overload overload = new Overload();
+            overload.Display();
+            Console.ReadKey();
+        }
+    }
+}
