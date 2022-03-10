0703086103bae2e6e7dfdbabd1adeeaa251db197
Alona Lutsenko <ext.Alona.Lutsenko@quorumsoftware.com>
2022-03-10 14:55:15 +0200

    first

--- ConsoleApp/Practice1/Overload.cs
+++ ConsoleApp/Practice1/Overload.cs
@@ -25,5 +25,14 @@ namespace ConsoleApp.Practice1
             System.Console.WriteLine(name);
             name = "Akhil 3";
         }
+        private void Display3(ref string x, ref string y)
+        {
+            System.Console.WriteLine(name);
+            x = "Akhil 1";
+            System.Console.WriteLine(name);
+            y = "Akhil 2";
+            System.Console.WriteLine(name);
+            name = "Akhil 3";
+        }
     }
 }
