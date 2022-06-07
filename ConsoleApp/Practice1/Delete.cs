e884ee575c95b38f3736a2c8708cad79b9a664c8
Alona Lutsenko <ext.Alona.Lutsenko@quorumsoftware.com>
2022-06-07 15:58:10 +0300

    Should be removed.

--- ConsoleApp/Practice1/Delete.cs
+++ ConsoleApp/Practice1/Delete.cs
@@ -8,5 +8,9 @@ namespace ConsoleApp.Practice1
 {
     internal class Delete
     {
+        //        Should You Use reset or revert in Git?
+        //You should really only use reset if the commit being reset only exists locally.This command changes the commit history and it might overwrite history that remote team members depend on.
+
+        //revert instead creates a new commit that undoes the changes, so if the commit to revert has already been pushed to a shared repository, it is best to use revert as it doesn't overwrite commit history.
     }
 }
