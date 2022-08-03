0ac55be2cd01271069f29bcba3a2376872097fb8
Alona Lutsenko <ext.Alona.Lutsenko@quorumsoftware.com>
2022-08-03 15:01:32 +0300

    Mast be removed

--- ConsoleApp/Practice1/Delete.cs
+++ ConsoleApp/Practice1/Delete.cs
@@ -8,5 +8,14 @@ namespace ConsoleApp.Practice1
 {
     internal class Delete
     {
+        String str = "GeeksForGeeks";
+
+        Console.WriteLine("String    : " + str);
+  
+        // retrieve the substring from index 0 to length 8
+        Console.WriteLine("Sub String1: " + str.Substring(0, 8));
+  
+        // retrieve the substring from index 5 to length 3
+        Console.WriteLine("Sub String2: " + str.Substring(5, 3));
     }
 }
